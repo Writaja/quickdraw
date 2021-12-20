3517919c3279c7caa4b68ec4aa921815984da26d
Writaja <74670011+Writaja@users.noreply.github.com>
2021-12-20 18:57:26 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -11,33 +11,32 @@ function preload(){
     
     }
     function draw(){
-    check_sketch()
-    if(draw_sketch==sketch){
-        answer_holder="set";
-        score=score+1;
-        document.getElementById("score").innerHTML="Score: "+score;
+    
 
     }
-    }
+    
     function clear_canvas(){
         background("white");
     }
-    function sketch_check(){
-timer_counter++;
-document.getElementById("timer").innerHTML="Timer: "+timer_counter;
-if(timer_counter>400){
-    timer_counter=0;
-}
-   if(timer_check=="completed"|| answer_holder="set"){
-        timer_check="";
-        answer_holder="";
-        updateCanvas();
-   }
-    }
+    
     function updateCanvas(){
         background="white";
         sketch=Element_of_array;
         document.getElementById("sketch_to_be_drawn").innerHTML="Sketch to be drawn"+sketch;
+    }
+    function classifyCanvas(){
+        classifier.classify(canvas,gotresult);
+   
+    }
+    function gotresult(error,results){
+   if(error){
+       console.error(error);
+   }
+   console.log(results);
+   document.getElementById("label").innerHTML="label: "+results[0].label;
+   document.getElementById("confidence").innerHTML="confidence: "+Math.round(results[0].confidence*100)+"%";
+    utterThis=new SpeechSynthesisUtterance(result[0].label);
+    synth.speak(utterThis);
     }
     
     
